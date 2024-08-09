3262fcaee604d3e517ad6cba72803cde247745f2
Hamza Boukader <boukaderhamza71@gmail.com>
2024-08-09 10:58:14 +0100

    Update signin/page.jsx

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -8,6 +8,10 @@ import { EmailSignIn,  GoogleSignUporIn } from '@/Firebase/Authentication';
 import { auth } from '@/Firebase/Initialisation';
 
 const SignIn = () => {
+  useLayoutEffect(()=>{
+        
+    auth.currentUser ? window.location.assign("/")  : null
+},[auth.currentUser])
  
   const [errors, setErrors] = useState({
         email: "",  
@@ -105,11 +109,11 @@ const SignIn = () => {
                                 <p className="text-red-500 text-sm mt-1">{error}</p>
                             )}
               <div className="!mt-8">
-                <button onClick={async()=>{await handleSignIn();window.location.assign("/")}} type="button" className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-hardBeige hover:bg-tooHardBeige transition-all duration-500 focus:outline-none">
+                <button onClick={async()=>{await handleSignIn()} type="button" className="w-full py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-hardBeige hover:bg-tooHardBeige transition-all duration-500 focus:outline-none">
                   Sign in
                 </button>
               </div>
-              <button onClick={async()=>{await GoogleSignUporIn();window.location.assign("/")}} href="#" className="flex items-center justify-center px-6 py-3 mt-4 text-gray-900 transition-colors duration-300 border-hardBeige transform border rounded-lg  hover:bg-softBeige">
+              <button onClick={async()=>{await GoogleSignUporIn()}} href="#" className="flex items-center justify-center px-6 py-3 mt-4 text-gray-900 transition-colors duration-300 border-hardBeige transform border rounded-lg  hover:bg-softBeige">
                     <svg className="w-6 h-6 mx-2" viewBox="0 0 40 40">
                         
                         <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
