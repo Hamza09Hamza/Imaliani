ef61e70103a8dd6ea826c1b183359ca9dc12bae4
Hamza Boukader <boukaderhamza71@gmail.com>
2024-08-09 11:03:35 +0100

    Update page.jsx

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useLayoutEffect } from 'react'
 import Logo from "@/images/imalian.png"
 import Image from 'next/image';
 import InputField from "@/components/Signing/InputFeild";
